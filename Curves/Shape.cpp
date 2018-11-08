8fd315bf4c63bd6f0d28f97e4f0211f34acc720b
redSkywalker <kira.zhzh@gmail.com>
2018-11-08 19:26:31 +0300

    Update Shape.cpp
    добавлена реализация статического метода make_shape класса Shape - Factory method

--- Curves/Shape.cpp
+++ Curves/Shape.cpp
@@ -1,3 +1,29 @@
 #include "stdafx.h"
 #include "Shape.h"
+#include "Circle.h"
+#include "Ellipse.h"
+#include "Helix.h"
 
+
+//Factory method
+		shared_ptr<Shape> Shape::make_shape(int rand)
+		{
+			switch (rand)
+			{
+				case 1:
+				{
+					return make_shared<Circle>();
+					break;
+				}
+				case 2:
+				{
+					return make_shared<Ellipse>();
+					break;
+				}
+				case 3:
+				{
+					return make_shared<Helix>();
+					break;
+				}
+			}
+		}
