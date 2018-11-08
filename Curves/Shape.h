2b92e0eec1f2f95e6b3cd9246b862bfadbb0482c
redSkywalker <kira.zhzh@gmail.com>
2018-11-08 19:23:40 +0300

    Update Shape.h
    добавлено объявление статического метода make_shape

--- Curves/Shape.h
+++ Curves/Shape.h
@@ -20,6 +20,10 @@ public:
 	virtual const string& GetType() = 0;	
 	virtual void GetPointValue(double t, double& x, double& y, double& z) = 0;
 	virtual void GetDerivativeValue(double t, double& x, double& y, double& z) = 0;
+	
+	//Factory method
+	static shared_ptr<Shape> make_shape(int rand);
+	
 protected:
 	virtual void Round(double& parametr) = 0;
 };
